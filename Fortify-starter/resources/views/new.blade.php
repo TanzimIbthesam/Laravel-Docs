2d78455d6f01ffc42aea5748ceb94ccf69f562fa
iamtanzim <tanzim67@gmail.com>
2020-09-30 18:57:56 +0600

    LaravelFortify without Jetstream guide

---
+++ Fortify-starter/resources/views/new.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    {{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
+
+</head>
+<body>
+{{-- <h1 class="text-2xl text-red-900">Hello World</h1> --}}
+<h1 class="display-2 lead text-danger">Display 4</h1>
+{{-- <h1 class="text-2xl text-red-900">Hello World</h1> --}}
+
+
+
+</body>
+</html>
---
+++ Fortify-starter/webpack.mix.js
@@ -0,0 +1,12 @@
+const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+//for tailwind
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/css/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')],
+    });
+//For bootstrap
+mix
+    .sass('resources/css/apptwo.scss', 'public/css')
