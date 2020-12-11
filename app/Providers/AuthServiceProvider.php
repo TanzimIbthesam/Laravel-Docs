6ea5baa1cf6adb9f7c932b7b3a9dfb3a832b219e
iamtanzim <tanzim67@gmail.com>
2020-12-11 11:45:41 +0600

    GATE update and delete

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,14 +25,37 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         //define an ability that someone can update a blog post
-        Gate::define('update-post',function($user,$post){
-            return $user->id==$post->user_id;
+        Gate::define('update-post',function($user,$blog){
+            return $user->id==$blog->user_id;
 
         });
-        Gate::define('delete-post',function($user,$post){
-            return $user->id==$post->user_id;
+        Gate::define('delete-post',function($user,$blog){
+            return $user->id==$blog->user_id;
 
         });
+        //If we want admin to enjoy all privilages 
+        Gate::before(function ($user, $ability) {
+            // if ($user->is_admin && in_array($ability, ['update-post'])) {
+            //     return true;
+            // }
+            if ($user->is_admin) {
+                return true;
+            }
+        });
+      //If we want admin to enoy any specific privilages 
+        Gate::before(function($user,$ability){
+            if($user->is_admin && in_array($ability,['update-post'])){
+                return true;
+            }
+
+        });
+        //after gate check
+        // Gate::after(function($user,$ability,$result){
+        //     if($user->is_admin ){
+        //         return true;
+        //     }
+
+        // });
 
         //
     }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,7 +16,8 @@ class UsersTableSeeder extends Seeder
     {
         //
         $usersCount=(int)$this->command->ask('How many users you would like?',10);
+        $defaultUser = User::factory(1)->defaultUser()->create();
         User::factory($usersCount)
-        ->create();
+        ->create()->concat($defaultUser);
     }
 }
